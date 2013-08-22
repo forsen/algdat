75f57ee5f614dd61de3ff81278b9a45fa8e42c96
Erik Haider Forsen <erik@forsen.no>
2013-08-22 14:20:10 +0200

    Forelesning 220813

--- src/no/forsen/algdat/Main.java
+++ src/no/forsen/algdat/Main.java
@@ -9,14 +9,21 @@ public class Main
 
 	public static void main(String[] args)
 	{
-		long tid = System.currentTimeMillis();
-		int[] a = Tabell.randPerm(1000000);
-		tid = System.currentTimeMillis() - tid;
+		int[] a = Tabell.randPerm(20);
+		System.out.println(Arrays.toString(a));
 
-		int m = Tabell.maks(a, -1, 5);
+		int m = Tabell.maks(a); 
 
+		Tabell.bytt(a, m, 0 );
+
+		int nm = Tabell.maks(a, 1, a.length); 
 	
+		if( nm == m )
+			nm = 0;
 
-		System.out.println(tid);
+		Tabell.bytt(a, m, 0);
+
+		System.out.println(m);
+		System.out.println(nm );
 	}
 }
--- src/no/forsen/hjelpeklasser/Tabell.java
+++ src/no/forsen/hjelpeklasser/Tabell.java
@@ -61,6 +61,20 @@ public class Tabell
 		return m;
 
 	} 
+	
+	public static int maks( int[] a )
+	{
+		if ( a.length < 1 )
+			throw new java.util.NoSuchElementException("a er tom");
+
+		int m = 0;
+
+		for( int i = 1; i < a.length; i++ )
+			if( a[i] >= a[m] )
+				m = i; 
+
+		return m;
+	}
 
 
 }
