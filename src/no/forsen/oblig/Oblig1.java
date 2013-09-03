aa5a07017a2d967d39168a4c7df107322e88ae4a
Erik Haider Forsén <erik@forsen.no>
2013-09-03 15:19:11 +0200

    Begynt paa oblig1

---
+++ src/no/forsen/oblig/Oblig1.java
@@ -0,0 +1,70 @@
+package no.forsen.oblig; 
+
+import java.util.NoSuchElementException;
+
+public class Oblig1
+{
+	public static int maks( int[] a )
+	{
+		int n = a.length; 
+		if( n < 1 )
+			throw new NoSuchElementException("Denne tabellen er tom");
+
+		if( n == 1 )
+			return a[0];
+
+		for( int i = 0; i < n - 1; i++ )
+		{
+			if( a[i] > a[i+1] )
+			{
+				int temp = a[i];
+				a[i] = a[i+1];
+				a[i+1] = temp;
+			}
+		}
+
+		return a[n - 1];
+	}
+	// Det vil bli gjennomført n - 1 sammenligninger i metoden over.
+	// Det blir færrest ombyttinger når tabellen er sortert stigende.
+	// Det blir flest ombyttinger når den største verdien ligger først i tabellen. Resten av verdiene spiller ingen rolle, 
+	// da den største verdien vil bli flyttet alle plassene uansett.
+
+	public static void sortering( int[] a )
+	{
+		int n = a.length;
+
+		if( n <= 1 )
+			return; 
+
+		for( int i = n; i > 0; i-- )
+		{
+			for( int j = 0; j < i - 1 ; j++ )
+			{
+				if( a[j] > a[j + 1])
+				{
+					int temp = a[j];
+					a[j] = a[j + 1];
+					a[j + 1] = temp; 
+				}
+			}
+		}
+	}
+	// For en tabell med lengde n vil det bli (n^2 - n) / 2 sammenligninger.
+
+	public static int antallUlikeSortert( int[] a )
+	{
+		for( int i = 0; i < a.length - 1; i++ )
+		{
+			if( a[i] > a[i+1] )
+				throw new IllegalStateException("Tabellen må være sortert");
+			
+			//while( a[i] == a[i+1] )
+
+		}
+
+		return 0;
+	}
+}
+
+
