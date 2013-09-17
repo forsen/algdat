a7b0e10988b7c2e35cfb195e8eee52f69e683550
Erik Haider Forsén <erik@forsen.no>
2013-09-17 12:30:19 +0200

    mere nytt

--- src/no/forsen/algdat/Program.java
+++ src/no/forsen/algdat/Program.java
@@ -11,16 +11,9 @@ public class Program
 	
 	public static void main(String[] args)
 	{
-		Heltall a = new Heltall(5);
-		Heltall b = new Heltall(5);
 
-		List<Heltall> liste = new ArrayList<>();
-		liste.add(a);
-
-		boolean x = a.compareTo(b) == 0;
-		boolean y = a.equals(b);
-
-		System.out.println( x + " " + y );
+		Person p = new Person("Kari", "Olsen");
+		System.out.println(p);
 
 	}
 }
--- src/no/forsen/hjelpeklasser/Person.java
+++ src/no/forsen/hjelpeklasser/Person.java
@@ -10,4 +10,38 @@ public class Person implements Comparable<Person>
 		this.fornavn = fornavn;
 		this.etternavn = etternavn; 
 	}
+
+	public String fornavn() 
+	{
+		return fornavn;
+	}
+
+	public String etternavn()
+	{
+		return etternavn;
+	}
+
+	public int compareTo( Person p )
+	{
+		int cmp = etternavn.compareTo( p.etternavn );
+		
+		if( cmp != 0 )
+			return cmp; 
+		return fornavn.compareTo(p.fornavn); 
+	}
+
+	public boolean equals( Object o )
+	{
+		if( this == o )
+			return true; 
+		if( !(o instanceof Person ) )
+			return false; 
+
+		return etternavn == ((Person).o).etternavn && fornavn == ((Person)o).fornavn;
+	}
+
+	public String toString()
+	{
+		return fornavn + " " + etternavn;
+	}
 }
