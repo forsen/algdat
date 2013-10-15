6469491a33cb04930fab783bf95b78c4e6ea385f
Erik Haider Forsén <erik@forsen.no>
2013-10-15 15:20:35 +0200

    Oppgave 4.1.2:4

--- src/no/forsen/algdat/Program.java
+++ src/no/forsen/algdat/Program.java
@@ -14,17 +14,42 @@ public class Program
 	public static void main( String[] args )
 	{
 		Stakk<Integer> a = new TabellStakk<>(4);
-
+		Stakk<Integer> b = new TabellStakk<>(4);
 		a.leggInn( 4 );
 		a.leggInn( 3 );
 		a.leggInn( 2 );
 		a.leggInn( 1 );
 
 		System.out.println( a );
+		System.out.println( b );
 
-		snu( a );
+		kopier( a, b );
 
 		System.out.println( a );
+		System.out.println( b );
+	}
+
+	public static <T> void kopier( Stakk<T> a, Stakk<T> b )
+	{
+		int størrelse = a.antall(); 
+		Stakk<T> c = new TabellStakk<>(størrelse);
+
+		for( int i = 0; i < størrelse; i++ )
+		{
+			T verdi = a.taUt(); 
+			b.leggInn( verdi );
+			c.leggInn( verdi ); 
+		}
+
+		for( int i = 0; i < størrelse; i++ ) 
+			a.leggInn( b.taUt() );
+		
+		for( int i = 0; i < størrelse; i++ )
+			b.leggInn( c.taUt() );
+
+
+
+
 	}
 
 	public static <T> void snu( Stakk<T> a )
