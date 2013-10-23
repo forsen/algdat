22e3aa528abd9606019c7de8ddcfea25d1403d2e
Erik Haider Forsén <erik@forsen.no>
2013-10-23 19:32:18 +0200

    Oppgave 4.1.2:7 + fikset snu()

--- src/main/java/no/forsen/algdat/Program.java
+++ src/main/java/no/forsen/algdat/Program.java
@@ -13,24 +13,40 @@ public class Program
 {
 	public static void main( String[] args )
 	{
-		Stakk<Integer> a = new TabellStakk<>(4);
-		Stakk<Integer> b = new TabellStakk<>(3);
-		a.leggInn( 4 );
-		a.leggInn( 3 );
-		a.leggInn( 2 );
-		a.leggInn( 1 );
+		Stakk<String> a = new TabellStakk<>(4);
+		
+		a.leggInn( "1234" );
+		a.leggInn( "123" );
+		a.leggInn( "12345" );
+		a.leggInn( "12" );
+		a.leggInn( "123456789");
+		a.leggInn( "123456");
+		a.leggInn( "1234567");
+		a.leggInn( "12345678");
+
+/*
+		a.leggInn( "12345" );
+		a.leggInn( "123" );
+		a.leggInn( "1234" );
+		a.leggInn( "12" );*/
+
+		Comparator c = new StrenglengdeKomparator(); 
 
 		System.out.println( a );
-		snu( a );
+		//snu(a);
+		//System.out.println( c.compare("12345","1234") );
+		sorter( a, c );
 		System.out.println( a );
 	}
 
 	public static <T> void kopier( Stakk<T> a, Stakk<T> b )
 	{
 		int størrelse = a.antall(); 
-		Stakk<T> c = new TabellStakk<>(størrelse);
+		
 
-		for( int i = 0; i < størrelse; i++ )
+
+
+		/*for( int i = 0; i < størrelse; i++ )
 		{
 			T verdi = a.taUt(); 
 			b.leggInn( verdi );
@@ -41,25 +57,67 @@ public class Program
 			a.leggInn( b.taUt() );
 		
 		for( int i = 0; i < størrelse; i++ )
-			b.leggInn( c.taUt() );
+			b.leggInn( c.taUt() );*/
+	}
+
+	public static <T> void sorter( Stakk<T> a, Comparator<? super T> c )
+	{
+		int antall = a.antall(); 
+		Stakk<T> b = new TabellStakk<>(antall);
+		boolean endring = true;  
+		while( endring )
+		{
+			endring = false; 
+			T maks = a.taUt();
+			T verdi = a.taUt(); 
+	
+			for( int i = 0; i < antall-2; i++ ) 
+			{
+				if( c.compare(maks,verdi) > 0 )
+				{
+					b.leggInn( maks );
+					maks = verdi;
+					verdi = a.taUt();
+				}
+				else
+				{
+					b.leggInn( verdi );
+					verdi = a.taUt(); 
+					endring = true; 	
+				}
+			}
+			if( c.compare(maks,verdi) > 0)
+			{
+				b.leggInn( maks );
+				b.leggInn( verdi );
+			}
+			else
+			{
+				b.leggInn( verdi );
+				b.leggInn( maks );
+			}
+			for( int i = 0; i < antall; i++ ) 
+				a.leggInn( b.taUt() );
+		}
 	}
 
 	public static <T> void snu( Stakk<T> a )
 	{
 		int størrelse = a.antall(); 
 		Stakk<T> b = new TabellStakk<T>( størrelse );
-		
+		 		
 		for( int i = 1; i < størrelse; i++ )
 		{
+			for( int j = 0; j < i ; j++ )
+			{
+				b.leggInn( a.taUt() );
+			}
 			T verdi = a.taUt(); 
 
-			for( int j = 0; j < størrelse - 1; j++ )
-				b.leggInn( a.taUt() );
 
-			a.leggInn( verdi ); 
-
-			for( int j = 0; j < størrelse - 1; j++)
+			for( int j = 0; j < i; j++)
 				a.leggInn( b.taUt() );
+			a.leggInn( verdi );
 		}
 	}
 
