bb9a9fc03161da1024150124940b0e7b027fe555
Erik Haider Forsén <erik@forsen.no>
2013-10-23 20:18:34 +0200

    Oppgave 4.2.2:5

---
+++ src/main/java/no/forsen/hjelpeklasser/TabellKø.java
@@ -0,0 +1,63 @@
+public class TabellKø<T> implements Kø<T>
+{
+	private T[] a; 
+	private int fra;
+	private int til; 
+
+	public TabellKø( int lengde )
+	{
+		if( lengde < 1 )
+			throw new IllegalArgumentException( "Må ha positiv lengde!" );
+
+		a = (T[]) new Object[ lengde ]; 
+
+		fra = til = 0; 
+	}
+
+	public TabellKø()
+	{
+		this( 8 ); 
+	}
+
+	public int antall()
+	{
+		return fra <= til ? til - fra : a.length + til - fra, 
+	}
+
+	public void leggInn( T t )
+	{
+		a[til] = t; 
+		til++; 
+		if( til == a.length )
+			til = 0;
+		if( fra == til )
+			a = utvidTabell( 2*a.length );
+	}
+
+	public T[] utvidTabell( int lengde )
+	{
+		T[] b = (T[]) new Object[lengde];
+
+		System.arraycopy(a, fra, b, 0, a.length - fra );
+
+		System.arraycopy( a, 0, b, a.length - fra, fra );
+
+		fra = 0; 
+		til = a.length; 
+
+		return b; 
+	}
+
+	public taUt()
+	{
+		if( fra == til )
+			throw new NoSuchElementException( "Køen er tom!" );
+
+		T temp = a[fra];
+		a[fra] = null; 
+		fra++;
+		if( fra == a.length )
+			fra = 0; 
+		return temp;
+	}
+}
