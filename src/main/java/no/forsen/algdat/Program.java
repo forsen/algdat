ecc046208febcdabe7e1160fccab900af7d51052
Erik Haider Forsén <erik@forsen.no>
2013-10-23 21:05:39 +0200

    Oppgave 4.2.2:9

--- src/main/java/no/forsen/algdat/Program.java
+++ src/main/java/no/forsen/algdat/Program.java
@@ -13,127 +13,29 @@ public class Program
 {
 	public static void main( String[] args )
 	{
-		long tid = System.currentTimeMillis();
+		Stakk<Integer> a = new TabellStakk<>(5);
 
-		Stakk<Integer> s = new LenketStakk<>();
-		Integer k = 0; 
+		a.leggInn(1);
+		a.leggInn(2);
+		a.leggInn(3);
+		a.leggInn(4);
+		a.leggInn(5);
 
-		for( int i = 0; i < 10000000; i++ ) 
-		{
-			s.leggInn( k );
-		}
-
-		while( !s.tom() )
-			s.taUt();
-
-		tid = System.currentTimeMillis() - tid; 
-		System.out.println( tid );
-
-	}
-
-	public static <T> void kopier( Stakk<T> a, Stakk<T> b )
-	{
-		int størrelse = a.antall(); 
-		
-
-
-
-		/*for( int i = 0; i < størrelse; i++ )
-		{
-			T verdi = a.taUt(); 
-			b.leggInn( verdi );
-			c.leggInn( verdi ); 
-		}
-
-		for( int i = 0; i < størrelse; i++ ) 
-			a.leggInn( b.taUt() );
-		
-		for( int i = 0; i < størrelse; i++ )
-			b.leggInn( c.taUt() );*/
-	}
-
-	public static boolean sjekkParanteser( String s, char v, char h )
-	{
-		Stakk<Integer> a = new TabellStakk<>(); 
-		try
-		{
-
-			for( int i = 0; i < s.length(); i++ )
-			{
-				if( s.charAt(i) == v )
-					a.leggInn(1);
-				else if ( s.charAt(i) == h )
-					a.taUt();
-			}
-		}
-		catch( Exception e )
-		{
-			return false;
-		}
-
-		return a.tom();
-	}
-
-	public static <T> void sorter( Stakk<T> a, Comparator<? super T> c )
-	{
-		int antall = a.antall(); 
-		Stakk<T> b = new TabellStakk<>(antall);
-		boolean endring = true;  
-		while( endring )
-		{
-			endring = false; 
-			T maks = a.taUt();
-			T verdi = a.taUt(); 
-	
-			for( int i = 0; i < antall-2; i++ ) 
-			{
-				if( c.compare(maks,verdi) > 0 )
-				{
-					b.leggInn( maks );
-					maks = verdi;
-					verdi = a.taUt();
-				}
-				else
-				{
-					b.leggInn( verdi );
-					verdi = a.taUt(); 
-					endring = true; 	
-				}
-			}
-			if( c.compare(maks,verdi) > 0)
-			{
-				b.leggInn( maks );
-				b.leggInn( verdi );
-			}
-			else
-			{
-				b.leggInn( verdi );
-				b.leggInn( maks );
-			}
-			for( int i = 0; i < antall; i++ ) 
-				a.leggInn( b.taUt() );
-		}
+		System.out.println( a );
+		snu( a );
+		System.out.println( a );
 	}
 
 	public static <T> void snu( Stakk<T> a )
 	{
-		int størrelse = a.antall(); 
-		Stakk<T> b = new TabellStakk<T>( størrelse );
-		 		
-		for( int i = 1; i < størrelse; i++ )
-		{
-			for( int j = 0; j < i ; j++ )
-			{
-				b.leggInn( a.taUt() );
-			}
-			T verdi = a.taUt(); 
+		int antall = a.antall(); 
+		Ko<T> b = new TabellKo<>(antall); 
 
-
-			for( int j = 0; j < i; j++)
-				a.leggInn( b.taUt() );
-			a.leggInn( verdi );
-		}
+		for( int i = 0; i < antall; i++ ) 
+			b.leggInn( a.taUt() );
+		
+		for( int i = 0; i < antall; i++ )
+			a.leggInn( b.taUt() );
 	}
-
 
 }
