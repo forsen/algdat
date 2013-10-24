057f795eb3a5ad90ad0c92d659ad0d94b5476378
Erik Haider Forsén <erik@forsen.no>
2013-10-24 14:51:47 +0200

    Oppgave 5.1.3:10

--- src/main/java/no/forsen/algdat/Program.java
+++ src/main/java/no/forsen/algdat/Program.java
@@ -13,54 +13,40 @@ public class Program
 {
 	public static void main( String[] args )
 	{
-		Ko<Integer> a = new TabellKo<>(100);
-
-		a.leggInn(1);
-		a.leggInn(2);
-		a.leggInn(3);
-		a.leggInn(4);
-		a.leggInn(5);
-
-		System.out.println( a );
-		snu( a );
-		System.out.println( a );
+		int[] a = {3,1,2,5,4,6,8,12,13};
+		System.out.println( girBinærtre( a ) );
 	}
 
-	public static <T> void snu( Stakk<T> a )
+	public static boolean girBinærtre( int[] a )
 	{
-		int antall = a.antall(); 
-		Ko<T> b = new TabellKo<>(antall); 
+		if( a.length == 0 )
+			return true; 
 
-		for( int i = 0; i < antall; i++ ) 
-			b.leggInn( a.taUt() );
-		
-		for( int i = 0; i < antall; i++ )
-			a.leggInn( b.taUt() );
-	}
+		Arrays.sort( a );
 
-	public static <T> void snu( Ko<T> a )
-	{
-		int antall = a.antall(); 
-		Ko<T> b = new TabellKo<>(100);
+		if( a[0] != 1 )
+			return false; 
 
-		int j = antall - 1;  
-		T verdi = null; 
-		while( j > 0 )
+		for( int i = 0; i < a.length - 1; i++ )
 		{
-			for(int i = 0; i < j + 1; i++ )
-			{
-				if( i == j )
-					verdi = a.taUt(); 
-				else
-					b.leggInn(a.taUt());
-			}
+			if( a[i+1] == a[i] )
+				return false; 
 
-			a.leggInn( verdi );
-			for( int i = 0; i < j; i++ )
-				a.leggInn( b.taUt() );
-			j--;
 		}
 
-	}
+		for( int i = 1; i < a.length; i++ )
+		{
+			int j = i - 1; 
+			int forelder = a[i]/2; 
 
+			for(; j >= 0; j-- )
+				if( forelder == a[j] )
+					break; 
+
+			if( j < 0 )
+				return false; 
+		}
+
+		return true;
+	}
 }
