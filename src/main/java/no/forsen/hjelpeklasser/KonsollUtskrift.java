c072bbc5c225b7685607dee66983714320878c8f
Erik Haider Forsén <erik@forsen.no>
2013-10-28 18:39:06 +0100

    Oppgave 5.1.6:5

--- src/main/java/no/forsen/hjelpeklasser/KonsollUtskrift.java
+++ src/main/java/no/forsen/hjelpeklasser/KonsollUtskrift.java
@@ -2,8 +2,18 @@ package no.forsen.hjelpeklasser;
 
 public class KonsollUtskrift<T> implements Oppgave<T>
 {
+	private int n, antall; 
+
+	public KonsollUtskrift( int n )
+	{
+		this.n = n; 
+		antall = 0; 
+	}
+
 	public void utførOppgave( T verdi )
 	{
-		System.out.println( verdi + " " );
+		System.out.print( verdi + " " );
+		if( ++antall % n == 0 )
+			System.out.println();
 	}
 }
