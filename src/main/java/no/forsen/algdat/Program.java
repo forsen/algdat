204e4b924efc68e1db62c0a1bcb612b5a3da3c85
Erik Haider Forsén <erik@forsen.no>
2013-10-28 10:12:54 +0100

    5.1.5:2

--- src/main/java/no/forsen/algdat/Program.java
+++ src/main/java/no/forsen/algdat/Program.java
@@ -18,5 +18,12 @@ public class Program
 		Character[] b = {'D','I','H','L','O','B','A','E','N','G','K','M','J','F','C'};
 
 		BinTre<Character> tre = new BinTre<>( a, b ); 
+
+		int[] c = {1,2,3,4,5,6,7,10,11,13,14,22,23,28,29};
+		Character[] d = {'E','I','B','G','A','H','K','L','O','D','N','M','C','J','F'};
+
+		BinTre<Character> tre2 = new BinTre<>( c, d );
+
+		System.out.println( tre2 );
 	}
 }
