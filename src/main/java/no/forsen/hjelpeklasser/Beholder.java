3b4482025086e8d967729dffa5f7e93d6ddd6b73
Erik Haider Forsén <erik@forsen.no>
2013-11-12 10:51:39 +0100

    javadoc

--- src/main/java/no/forsen/hjelpeklasser/Beholder.java
+++ src/main/java/no/forsen/hjelpeklasser/Beholder.java
@@ -14,10 +14,35 @@ public interface Beholder<T> extends Iterable<T>
 	* @return resultatet av innleggingen
 	*/
     public boolean leggInn(T t);
+    /**
+     * Metode for å sjekke om en verdi finnes i beholderen
+     * @param t verdien som skal søkes etter
+     * @return true dersom den finnes, false ellers
+     */
     public boolean inneholder(T t);
+    /**
+     * Metode for å fjerne en verdi fra beholderen
+     * @param t verdien som skal fjernes
+     * @return true dersom verdien ble fjernet, false ellers
+     */
     public boolean fjern(T t);
+    /**
+     * Metode for å finne antall elementer i beholderen
+     * @return antall elementer i beholderen
+     */ 
     public int antall();
+    /**
+     * Metode for å avgjøre om beholderen er tom
+     * @return true dersom beholderen er tom, false ellers
+     */
     public boolean tom();
+    /**
+     * Metode for å nullstille beholderen
+     */
     public void nullstill();
+    /**
+     * En iterator for beholderen
+     * @return et Iterator object
+     */
     public Iterator<T> iterator();
 }
