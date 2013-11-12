c4a2cce1c17cc956a44881f03d65c29893d952d6
Erik Haider Forsén <erik@forsen.no>
2013-11-12 10:44:18 +0100

    javadoc

--- src/main/java/no/forsen/hjelpeklasser/Beholder.java
+++ src/main/java/no/forsen/hjelpeklasser/Beholder.java
@@ -1,8 +1,18 @@
+/**
+ * Et interface for stakker / køer o.l.
+ * @author Erik Haider Forsén
+ */
+
 package no.forsen.hjelpeklasser;
 
 import java.util.Iterator;
 public interface Beholder<T> extends Iterable<T> 
 {
+	/**
+	* Metode for å legge inn verdier i beholderen
+	* @param t verdien som skal legges inn
+	* @return resultatet av innleggingen
+	*/
     public boolean leggInn(T t);
     public boolean inneholder(T t);
     public boolean fjern(T t);
